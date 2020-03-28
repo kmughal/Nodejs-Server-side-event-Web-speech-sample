1a33a7b05eabdcd005d797d26a6b4107784ba258
Khurram <khurramshahzad@tfl.gov.uk>
2020-03-28 15:21:15 +0000

    add figlet

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
-
+const figlet = require('figlet')
 const { applyMiddlewares } = require('./middlewares')
 const { addRoutes } = require('./routes')
 
 applyMiddlewares(app)
 addRoutes(app)
+
 const portNumber = process.env.PORT || 9300
 app.listen(portNumber, () => {
-  console.log('connected : ' , portNumber)
+  
+  figlet('Khurram Shahzad', (e, r) => {
+    console.log(r)
+  })
+  console.log('connected : ', portNumber)
 })
